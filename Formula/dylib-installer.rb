99ec0604451f57a3636a6380ce883d76a629a306
hackerchai <i@hackerchai.com>
2024-07-10 16:56:18 +0800

    feat: bump version
    Signed-off-by: hackerchai <i@hackerchai.com>

--- Formula/dylib-installer.rb
+++ Formula/dylib-installer.rb
@@ -6,14 +6,14 @@ class DylibInstaller < Formula
   homepage "https://github.com/hackerchai/dylib-installer"
   url "https://github.com/hackerchai/dylib-installer"
   license "GPL-3.0"
-  version "0.1.0"
+  version "0.1.1"
 
   if Hardware::CPU.intel?
-    url "https://github.com/hackerchai/dylib-installer/releases/download/v0.1.0/dylib_installer-macos-amd64"
-    sha256 "a274e7f0aee3eeda51dc0862d237c69fc1d0c60dafebd682cf61e99eac3ab367"
+    url "https://github.com/hackerchai/dylib-installer/releases/download/v0.1.1/dylib_installer-macos-amd64"
+    sha256 "7d1a8c3ed7f20f2dbecee2e253cce19a482254edb1956d4242f7e4c2acca7f48"
   elsif Hardware::CPU.arm?
-    url "https://github.com/hackerchai/dylib-installer/releases/download/v0.1.0/dylib_installer-macos-arm64"
-    sha256 "f7c771eabbf8cb20e20b555ef0eb4a1ea8cd20ac522c3e3ed2149ceafba9e60f"
+    url "https://github.com/hackerchai/dylib-installer/releases/download/v0.1.1/dylib_installer-macos-arm64"
+    sha256 "a43e3ed79ccffcd6d22896bf24e200b76cb6c7b65bbcb791c3ad70e94f1bb65e"
   end
 
   def install
