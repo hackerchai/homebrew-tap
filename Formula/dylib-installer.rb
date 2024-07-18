7c1baa18a95fa1a64db24960977ed8939738ecc4
hackerchai <i@hackerchai.com>
2024-07-18 12:17:01 +0800

    chore: bump dylib_installer version 0.2.0
    Signed-off-by: hackerchai <i@hackerchai.com>

--- Formula/dylib-installer.rb
+++ Formula/dylib-installer.rb
@@ -5,14 +5,14 @@ class DylibInstaller < Formula
   desc "Tool to install dylib files and header to system library path written in Rust."
   homepage "https://github.com/hackerchai/dylib-installer"
   license "GPL-3.0"
-  version "0.1.1"
+  version "0.2.0"
 
   if Hardware::CPU.intel?
-    url "https://github.com/hackerchai/dylib-installer/releases/download/v0.1.1/dylib_installer-macos-amd64"
-    sha256 "7d1a8c3ed7f20f2dbecee2e253cce19a482254edb1956d4242f7e4c2acca7f48"
+    url "https://github.com/hackerchai/dylib-installer/releases/download/v0.2.0/dylib_installer-macos-amd64"
+    sha256 "8dbeae08fe0ecb4269701261d3cfb755f8f2d7803eb5fc8e44994b1df8e8abdc"
   elsif Hardware::CPU.arm?
-    url "https://github.com/hackerchai/dylib-installer/releases/download/v0.1.1/dylib_installer-macos-arm64"
-    sha256 "a43e3ed79ccffcd6d22896bf24e200b76cb6c7b65bbcb791c3ad70e94f1bb65e"
+    url "https://github.com/hackerchai/dylib-installer/releases/download/v0.2.0/dylib_installer-macos-arm64"
+    sha256 "d0b3a8fc3cb03c8dd4d4e2f40632880b71751b1bfeed4c2602fadbc5e6da1218"
   end
 
   def install
